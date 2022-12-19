c88411440bdc8e0ce9ab03002a7243967453b896
alizadeh-mohsen <kelarens@gmail.com>
2022-12-19 18:56:20 +0330

    Add migration and scaffold customer crud

---
+++ Customers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BatteryPeykCustomers.Data;
+using BatteryPeykCustomers.Model;
+
+namespace BatteryPeykCustomers.Customers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BatteryPeykCustomers.Data.ApplicationDbContext _context;
+
+        public IndexModel(BatteryPeykCustomers.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customer> Customer { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Customer != null)
+            {
+                Customer = await _context.Customer.ToListAsync();
+            }
+        }
+    }
+}
---
+++ Migrations/20221219151843_initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BatteryPeykCustomers.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Battery = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GuarantyStartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Comments = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
