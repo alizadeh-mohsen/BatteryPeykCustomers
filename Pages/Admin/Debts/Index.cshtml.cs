6b9504543433b62c81a17b751602a32833c01b6c
Mohsen Alizadeh <kelarens@gmail.com>
2025-01-18 12:14:22 +0000

    debt update

--- Pages/Admin/Debts/Index.cshtml.cs
+++ Pages/Admin/Debts/Index.cshtml.cs
@@ -2,12 +2,19 @@
 using Microsoft.EntityFrameworkCore;
 using BatteryPeykCustomers.Data;
 using BatteryPeykCustomers.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BatteryPeykCustomers.Pages.Admin.Debts
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchCommentString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public long TotalCredit { get; set; }
 
         public IndexModel(ApplicationDbContext context)
         {
@@ -18,10 +25,17 @@ namespace BatteryPeykCustomers.Pages.Admin.Debts
 
         public async Task OnGetAsync()
         {
-            Debt = await _context.Debt.OrderBy(C => C.Id)
-                //.Include(d => d.Counterparty)
-                //.Include(d => d.Reason).OrderBy(c => c.Counterparty.Title)
+            IQueryable<Debt> query = _context.Debt;
+            if (!string.IsNullOrEmpty(SearchCommentString))
+                query = query.Where(s => s.Description.Contains(SearchCommentString));
+
+                
+            Debt = await query.OrderBy(C => C.Id)
+                  //.Include(d => d.Counterparty)
+                  //.Include(d => d.Reason).OrderBy(c => c.Counterparty.Title)
                 .ToListAsync();
+
+            TotalCredit = Debt.Sum(s=>s.Amount);
         }
     }
 }
