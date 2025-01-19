6709bbf410adc8a68b452891b380ce58c28a209f
Mohsen Alizadeh <kelarens@gmail.com>
2025-01-19 11:47:15 +0000

    search added to guaranties

--- Pages/Admin/Guaranties/Index.cshtml.cs
+++ Pages/Admin/Guaranties/Index.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BatteryPeykCustomers.Data;
 using BatteryPeykCustomers.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Humanizer;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BatteryPeykCustomers.Pages.Admin.Guaranties
 {
@@ -14,18 +17,50 @@ namespace BatteryPeykCustomers.Pages.Admin.Guaranties
     {
         private readonly ApplicationDbContext _context;
 
+        public SelectList? CompanyList { get; set; }
+        public SelectList? AmperList { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SelectedCompany { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SelectedAmper { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? From { get; set; }
+
+        public IList<Guarranty> Guarranty { get; set; } = default!;
+
         public IndexModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public IList<Guarranty> Guarranty { get; set; } = default!;
 
-        public async Task OnGetAsync()
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            Guarranty = await _context.Guarranty
-                .Include(g => g.Amper)
-                .Include(g => g.Company).OrderBy(c => c.Id).ToListAsync();
+            AmperList = new SelectList(await _context.Amper.OrderBy(c => c.Amperage).ToListAsync(), "Id", "Title");
+            CompanyList = new SelectList(await _context.Company.OrderBy(c => c.Title).ToListAsync(), "Id", "Title");
+
+
+            IQueryable<Guarranty> result = _context.Guarranty
+                 .Include(g => g.Amper)
+                .Include(g => g.Company)
+                .OrderBy(c => c.Id);
+
+
+            if (!string.IsNullOrEmpty(SelectedCompany))
+                result = result.Where(t => t.CompanyId == int.Parse(SelectedCompany));
+
+            if (!string.IsNullOrEmpty(SelectedAmper))
+                result = result.Where(t => t.AmperId == int.Parse(SelectedAmper));
+
+            if (From != null)
+                result = result.Where(c => c.Date.Date == From);
+
+            Guarranty = await result.ToListAsync();
+            return Page();
         }
     }
 }
